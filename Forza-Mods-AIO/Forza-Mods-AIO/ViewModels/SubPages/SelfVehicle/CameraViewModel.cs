c79f34addfeb0d0623f95739d8a1beb4d6a5a5ce
szaaamerik <126014478+szaaamerik@users.noreply.github.com>
2024-04-13 08:24:07 -0700

    feat(fh4): camera page

--- Forza-Mods-AIO/Forza-Mods-AIO/ViewModels/SubPages/SelfVehicle/CameraViewModel.cs
+++ Forza-Mods-AIO/Forza-Mods-AIO/ViewModels/SubPages/SelfVehicle/CameraViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Forza_Mods_AIO.Models;
 
 namespace Forza_Mods_AIO.ViewModels.SubPages.SelfVehicle;
 
 public partial class CameraViewModel : ObservableObject
 {
+    public bool IsFh5 => GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5;
+    
     [ObservableProperty]
     private bool _areScanPromptLimiterUiElementsVisible = true;
     
--- Forza-Mods-AIO/Forza-Mods-AIO/Views/SubPages/SelfVehicle/Camera.xaml.cs
+++ Forza-Mods-AIO/Forza-Mods-AIO/Views/SubPages/SelfVehicle/Camera.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using Forza_Mods_AIO.Cheats.ForzaHorizon5;
+using Forza_Mods_AIO.Models;
 using Forza_Mods_AIO.ViewModels.SubPages.SelfVehicle;
 using MahApps.Metro.Controls;
 using static Forza_Mods_AIO.Resources.Memory;
@@ -20,57 +21,115 @@ public partial class Camera
     
     public CameraViewModel ViewModel { get; }
     private static CameraCheats CameraCheatsFh5 => Forza_Mods_AIO.Resources.Cheats.GetClass<CameraCheats>();
+    private static Cheats.ForzaHorizon4.CameraCheats CameraCheatsFh4 =>
+        Forza_Mods_AIO.Resources.Cheats.GetClass<Cheats.ForzaHorizon4.CameraCheats>();
 
     private async void LimitersScanButton_OnClick(object sender, RoutedEventArgs e)
     {
         ViewModel.AreScanPromptLimiterUiElementsVisible = false;
         ViewModel.AreScanningLimiterUiElementsVisible = true;
 
-        if (!CameraCheatsFh5.WereLimitersScanned)
+        bool successful;
+        switch (GameVerPlat.GetInstance().Type)
         {
-            await CameraCheatsFh5.CheatLimiters();
+            case GameVerPlat.GameType.Fh4:
+            {
+                if (!CameraCheatsFh4.WereLimitersScanned)
+                {
+                    await CameraCheatsFh4.CheatLimiters();
+                }
+
+                successful = CameraCheatsFh4.WereLimitersScanned;
+                break;
+            }
+            case GameVerPlat.GameType.Fh5:
+            {
+                if (!CameraCheatsFh5.WereLimitersScanned)
+                {
+                    await CameraCheatsFh5.CheatLimiters();
+                }
+
+                successful = CameraCheatsFh5.WereLimitersScanned;
+                break;
+            }
+            case GameVerPlat.GameType.None:
+            default:
+                throw new IndexOutOfRangeException();
         }
 
-        if (!CameraCheatsFh5.WereLimitersScanned)
+        if (!successful)
         {
             ViewModel.AreScanPromptLimiterUiElementsVisible = true;
             ViewModel.AreScanningLimiterUiElementsVisible = false;
             return;
         }
-
-        LimiterMinBox.Value = GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress);
-        LimiterMaxBox.Value = GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress + 4);
-
+        
+        var address = GameVerPlat.GetInstance().Type switch
+        {
+            GameVerPlat.GameType.Fh4 => CameraCheatsFh4.ChaseAddress,
+            GameVerPlat.GameType.Fh5 => CameraCheatsFh5.ChaseAddress,
+            _ => throw new IndexOutOfRangeException()
+        };
+        
+        LimiterMinBox.Value = GetInstance().ReadMemory<float>(address);
+        LimiterMaxBox.Value = GetInstance().ReadMemory<float>(address + 4);
+        
         ViewModel.AreScanningLimiterUiElementsVisible = false;
         ViewModel.AreLimiterUiElementsVisible = true;
     }
 
     private void LimiterComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        if (sender is not ComboBox comboBox || !CameraCheatsFh5.WereLimitersScanned)
+        var successful = CameraCheatsFh5.WereLimitersScanned || CameraCheatsFh4.WereLimitersScanned;
+        if (sender is not ComboBox comboBox || !successful)
         {
             return;
         }
 
-        LimiterMinBox.Value = comboBox.SelectedIndex switch
+        if (GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5)
         {
-            0 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress),
-            1 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseFarAddress),
-            2 => GetInstance().ReadMemory<float>(CameraCheatsFh5.DriverAddress),
-            3 => GetInstance().ReadMemory<float>(CameraCheatsFh5.HoodAddress),
-            4 => GetInstance().ReadMemory<float>(CameraCheatsFh5.BumperAddress),
-            _ => 0
-        };
-
-        LimiterMaxBox.Value = comboBox.SelectedIndex switch
+            LimiterMinBox.Value = comboBox.SelectedIndex switch
+            {
+                0 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress),
+                1 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseFarAddress),
+                2 => GetInstance().ReadMemory<float>(CameraCheatsFh5.DriverAddress),
+                3 => GetInstance().ReadMemory<float>(CameraCheatsFh5.HoodAddress),
+                4 => GetInstance().ReadMemory<float>(CameraCheatsFh5.BumperAddress),
+                _ => 0
+            };
+            
+            LimiterMaxBox.Value = comboBox.SelectedIndex switch
+            {
+                0 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress + 4),
+                1 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseFarAddress + 4),
+                2 => GetInstance().ReadMemory<float>(CameraCheatsFh5.DriverAddress + 4),
+                3 => GetInstance().ReadMemory<float>(CameraCheatsFh5.HoodAddress + 4),
+                4 => GetInstance().ReadMemory<float>(CameraCheatsFh5.BumperAddress + 4),
+                _ => 0
+            };
+        }
+        else
         {
-            0 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseAddress + 4),
-            1 => GetInstance().ReadMemory<float>(CameraCheatsFh5.ChaseFarAddress + 4),
-            2 => GetInstance().ReadMemory<float>(CameraCheatsFh5.DriverAddress + 4),
-            3 => GetInstance().ReadMemory<float>(CameraCheatsFh5.HoodAddress + 4),
-            4 => GetInstance().ReadMemory<float>(CameraCheatsFh5.BumperAddress + 4),
-            _ => 0
-        };
+            LimiterMinBox.Value = comboBox.SelectedIndex switch
+            {
+                0 => GetInstance().ReadMemory<float>(CameraCheatsFh4.ChaseAddress),
+                1 => GetInstance().ReadMemory<float>(CameraCheatsFh4.ChaseFarAddress),
+                2 => GetInstance().ReadMemory<float>(CameraCheatsFh4.DriverAddress),
+                3 => GetInstance().ReadMemory<float>(CameraCheatsFh4.HoodAddress),
+                4 => GetInstance().ReadMemory<float>(CameraCheatsFh4.BumperAddress),
+                _ => 0
+            };
+            
+            LimiterMaxBox.Value = comboBox.SelectedIndex switch
+            {
+                0 => GetInstance().ReadMemory<float>(CameraCheatsFh4.ChaseAddress + 4),
+                1 => GetInstance().ReadMemory<float>(CameraCheatsFh4.ChaseFarAddress + 4),
+                2 => GetInstance().ReadMemory<float>(CameraCheatsFh4.DriverAddress + 4),
+                3 => GetInstance().ReadMemory<float>(CameraCheatsFh4.HoodAddress + 4),
+                4 => GetInstance().ReadMemory<float>(CameraCheatsFh4.BumperAddress + 4),
+                _ => 0
+            };
+        }
     }
 
     private void LimiterMinBox_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double?> e)
@@ -85,27 +144,47 @@ public partial class Camera
         {
             case 0:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.ChaseAddress, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.ChaseAddress
+                            : CameraCheatsFh4.ChaseAddress, newValue);
                 break;
             }
             case 1:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.ChaseFarAddress, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.ChaseFarAddress
+                            : CameraCheatsFh4.ChaseFarAddress, newValue);
                 break;
             }
             case 2:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.DriverAddress, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.DriverAddress
+                            : CameraCheatsFh4.DriverAddress, newValue);
                 break;
             }
             case 3:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.HoodAddress, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.HoodAddress
+                            : CameraCheatsFh4.HoodAddress, newValue);
                 break;
             }
             case 4:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.BumperAddress, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.BumperAddress
+                            : CameraCheatsFh4.BumperAddress, newValue);
                 break;
             }
         }
@@ -123,27 +202,47 @@ public partial class Camera
         {
             case 0:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.ChaseAddress + 4, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.ChaseAddress + 4
+                            : CameraCheatsFh4.ChaseAddress + 4, newValue);
                 break;
             }
             case 1:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.ChaseFarAddress + 4, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.ChaseFarAddress + 4
+                            : CameraCheatsFh4.ChaseFarAddress + 4, newValue);
                 break;
             }
             case 2:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.DriverAddress + 4, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.DriverAddress + 4
+                            : CameraCheatsFh4.DriverAddress + 4, newValue);
                 break;
             }
             case 3:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.HoodAddress + 4, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.HoodAddress + 4
+                            : CameraCheatsFh4.HoodAddress + 4, newValue);
                 break;
             }
             case 4:
             {
-                GetInstance().WriteMemory(CameraCheatsFh5.BumperAddress + 4, newValue);
+                GetInstance()
+                    .WriteMemory(
+                        GameVerPlat.GetInstance().Type == GameVerPlat.GameType.Fh5
+                            ? CameraCheatsFh5.BumperAddress + 4
+                            : CameraCheatsFh4.BumperAddress + 4, newValue);
                 break;
             }
         }
