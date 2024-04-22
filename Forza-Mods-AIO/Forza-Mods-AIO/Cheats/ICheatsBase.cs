9202f0358d9c0e589b1e574882200a9f461d021c
Draff <comamnds@outlook.com>
2024-04-22 18:20:10 +0100

    wipe

--- Forza-Mods-AIO/Forza-Mods-AIO/Cheats/ICheatsBase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Forza_Mods_AIO.Cheats;
-
-public interface ICheatsBase
-{
-    public void Cleanup();
-    public void Reset();
-}
--- Forza-Mods-AIO/Forza-Mods-AIO/Models/DebugInfoReport.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Globalization;
-
-namespace Forza_Mods_AIO.Models;
-
-public class DebugInfoReport(string info)
-{
-    public string Info { get; } = info;
-    public string ReportedAt { get; } = DateTime.Now.ToString(CultureInfo.InvariantCulture);
-}
